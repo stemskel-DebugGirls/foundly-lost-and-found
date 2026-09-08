6487f922ef36fce314a0a1f553a439879f57c10c
Debug Girls <stemskel@gmail.com>
2026-09-08 11:01:23 +0800

    Update API URL for deployment

--- src/api.js
+++ src/api.js
@@ -1,3 +1,10 @@
-const API_URL = `http://${window.location.hostname}:5000`;
+const isLocal =
+  window.location.hostname === "localhost" ||
+  window.location.hostname === "127.0.0.1" ||
+  window.location.hostname.startsWith("192.168.");
+
+const API_URL = isLocal
+  ? `http://${window.location.hostname}:5000`
+  : "https://foundly-lost-and-found.onrender.com";
 
 export default API_URL;
